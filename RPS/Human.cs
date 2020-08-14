ada73343eaad5d1351a540c9ea8d8f023e68d6be
devDavidD <david.domask@outlook.com>
2020-08-14 15:20:25 -0500

    ran proj, Human Cs complete

--- RPS/Human.cs
+++ RPS/Human.cs
@@ -7,32 +7,14 @@ namespace RPS
 {
     class Human : Player
     {
-        //member variables (has a)
-        //Human has all member variables of Player clas because it inherits it
-
-        //ctor
         public Human()
         {
-            
-
-            name = "Aaron";
-           
-            
-
-
-
-            //member variable (can do)
-
+            name = "E";
         }
-
-        public override void ChooseMove(Human  )
+        public override void ChooseMove(  )
         {
-            //display choices - Console.WriteLine
-            //get user input - Console.ReadLine
             Console.WriteLine("rock", "paper", "scissors", "lizard", "spock");
-            Console.ReadLine();
-            //we want to take that user input and set this object's chosenGesture member variable to that string
-            //chosenGesture = our previous user input
+            string response = Console.ReadLine();
         }
     }
 }
